1c131ebfb16a1e1129fa384b5bbb08ecfc78cb1e
Richouf95 <wourichouf95@gmail.com>
2025-02-15 15:32:41 +0100

    Update serre page for sudos

--- src/app/dashboard/serres/page.tsx
+++ src/app/dashboard/serres/page.tsx
@@ -16,94 +16,18 @@ import { Plus } from "lucide-react";
 import Link from "next/link";
 import React, { useState } from "react";
 import serreIcon from "@/assets/icons/serre.svg";
+import SerresList from "@/components/serre/SerresList";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 
 const Serres = () => {
-  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const { serres } = useSelector((state: RootState) => state.serre);
+  console.log(serres)
   return (
     <div>
-      {/* Header */}
       <div className="flex justify-between items-center px-5 mt-5">
-        <h1 className="text-3xl font-bold">Liste des serres</h1>
-        <div className="flex items-center gap-3">
-          <span>Ajouter une serre</span>
-          <AlertDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-            <AlertDialogTrigger className="bg-primary text-white px-3 py-2 rounded-lg">
-              <Plus />
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>Ajouter une nouvelle serre</AlertDialogTitle>
-                <AlertDialogDescription>
-                  Veuillez remplir les informations nécessaires pour ajouter une
-                  nouvelle serre à votre liste.
-                </AlertDialogDescription>
-              </AlertDialogHeader>
-              <form className="flex flex-col gap-5">
-                <div className="text-xl">
-                  <Label htmlFor="nom" className="text-lg">
-                    Identifiant de la serre
-                  </Label>
-                  <Input
-                    id="nom"
-                    type="text"
-                    placeholder="serreId"
-                    required
-                    className="text-lg"
-                    // onChange={(e) => setCultureName(e.target.value)}
-                  />
-                </div>
-                <div className="">
-                  <Label htmlFor="type" className="text-lg">
-                    Identifiant des capteurs
-                  </Label>
-                  <Input
-                    id="type"
-                    type="text"
-                    placeholder="capteurId"
-                    required
-                    className="text-lg"
-                    // onChange={(e) => setCultureType(e.target.value)}
-                  />
-                </div>
-                <div className="">
-                  <Label htmlFor="variete" className="text-lg">
-                    Identifiant de la protenta
-                  </Label>
-                  <Input
-                    id="variete"
-                    type="text"
-                    placeholder="protentaId"
-                    required
-                    className="text-lg"
-                    // onChange={(e) => setCultureVariety(e.target.value)}
-                  />
-                </div>
-                <div className="">
-                  <Label htmlFor="variete" className="text-lg">
-                    Nom de la serre
-                  </Label>
-                  <Input
-                    id="variete"
-                    type="text"
-                    placeholder="Nom de la serre"
-                    required
-                    className="text-lg"
-                    // onChange={(e) => setCultureVariety(e.target.value)}
-                  />
-                </div>
-              </form>
-              <AlertDialogFooter>
-                <AlertDialogCancel>Annuler</AlertDialogCancel>
-                <button
-                  type="button"
-                  // onClick={handleNewCulture}
-                  className="bg-primary text-white px-4 py-2 rounded-lg"
-                >
-                  Ajouter
-                </button>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
+        <div>
+          <h1 className="text-3xl font-bold">Liste des serres</h1>
         </div>
       </div>
 
@@ -111,29 +35,7 @@ const Serres = () => {
         <hr />
       </div>
 
-      <div className="p-5 flex flex-wrap gap-8">
-        {[1, 2, 3, 4, 5].map((_, index) => (
-          <div
-            key={index}
-            className="flex flex-col max-w-xs rounded-lg items-center border shadow-xl bg-secondary hover:shadow-2xl"
-          >
-            <div className="flex justify-center bg-white p-5 rounded-tl-lg rounded-tr-lg">
-              <img src={serreIcon.src} className="w-1/2" alt="Serre icon"/>
-            </div>
-            <div className="w-full p-3">
-              <h2 className="text-2xl font-bold">Serre 1</h2>
-              <p className="text-lg">Serre descritption</p>
-              <p className="text-lg">Culture active : Culture 1</p>
-              <Link
-                href={"#"}
-                className="bg-primary block px-4 py-2 text-center text-white rounded-lg mt-3"
-              >
-                Ouvrire la serre
-              </Link>
-            </div>
-          </div>
-        ))}
-      </div>
+      <SerresList serres={serres ?? []} />
     </div>
   );
 };
--- src/components/serre/SerresList.tsx
+++ src/components/serre/SerresList.tsx
@@ -4,32 +4,45 @@ import Link from "next/link";
 import React from "react";
 import serreIcon from "@/assets/icons/serre.svg";
 import { ISerre } from "@/types/serre";
+import { ICulture } from "@/types/culture";
 
 const SerresList = ({ serres }: { serres: ISerre[] }) => {
   return (
     <div className="p-5 flex flex-wrap gap-8">
       {serres &&
-        serres.map((_, index) => (
-          <div
-            key={index}
-            className="flex flex-col max-w-xs rounded-lg items-center border shadow-xl bg-secondary hover:shadow-2xl"
-          >
-            <div className="flex justify-center bg-white p-5 rounded-tl-lg rounded-tr-lg">
-              <img src={serreIcon.src} className="w-1/2" alt="Serre Icon" />
+        serres.map((serre, index) => {
+          const allCulture = serre.allCulture
+          let activeCultureName = "N/A";
+
+          if (allCulture.length > 0) {
+            const active = allCulture.filter((c: ICulture) => !c.productionIsEnded)[0];
+            if (active) {
+              activeCultureName = active.name;
+            }
+          }
+          
+          return (
+            <div
+              key={index}
+              className="flex flex-col max-w-xs rounded-lg items-center border shadow-xl bg-secondary hover:shadow-2xl"
+            >
+              <div className="flex justify-center bg-white p-5 rounded-tl-lg rounded-tr-lg">
+                <img src={serreIcon.src} className="w-1/2" alt="Serre Icon" />
+              </div>
+              <div className="w-full p-3">
+                <h2 className="text-2xl font-bold">{serre.name}</h2>
+                {/* <p className="text-lg">Serre descritption</p> */}
+                <p className="text-lg">Culture active : {activeCultureName}</p>
+                <Link
+                  href={"#"}
+                  className="bg-primary block px-4 py-2 text-center text-white rounded-lg mt-3"
+                >
+                  Ouvrire la serre
+                </Link>
+              </div>
             </div>
-            <div className="w-full p-3">
-              <h2 className="text-2xl font-bold">Serre 1</h2>
-              {/* <p className="text-lg">Serre descritption</p> */}
-              <p className="text-lg">Culture active : Culture 1</p>
-              <Link
-                href={"#"}
-                className="bg-primary block px-4 py-2 text-center text-white rounded-lg mt-3"
-              >
-                Ouvrire la serre
-              </Link>
-            </div>
-          </div>
-        ))}
+          );
+        })}
     </div>
   );
 };
